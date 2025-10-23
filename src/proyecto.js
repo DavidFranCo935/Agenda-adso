3d90b62b6377e9729dd34f0f59bb2d1afdae4cda
David franco <davidfranco291@hotmail.com>
2025-10-22 23:13:09 -0500

    subiendo proyecto2 y ejercicio

---
+++ src/proyecto.js
@@ -0,0 +1,9 @@
+/*/const nombre = "Carolina";
+const notas = [3.5, 2.0, 2.8];
+
+const promedio = (notas[0] + notas[1] + notas[2]) / 3;
+
+console.log(`Aprendiz: ${nombre}`);
+console.log(`Promedio: ${promedio.toFixed(2)}`);
+console.log(`Estado: ${promedio >= 3.5 ? "Aprobado" : "No aprobado"}`);/*/
+
